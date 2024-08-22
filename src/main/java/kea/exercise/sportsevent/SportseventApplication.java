aaaead541c3966719a6ce1cb88886431edbb276e
Bransholm <kenneth@bransholm.dk>
2024-08-22 12:54:29 +0200

    App created

---
+++ src/main/java/kea/exercise/sportsevent/SportseventApplication.java
@@ -0,0 +1,13 @@
+package kea.exercise.sportsevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportseventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportseventApplication.class, args);
+    }
+
+}
